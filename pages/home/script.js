1749856787fa407833be52d5539efcfca4eb6950
XinPi <3eunguyen@protonmail.com>
2022-12-08 16:32:28 -0800

    Fanciest Header Sticking Animation Ever

--- pages/home/script.js
+++ pages/home/script.js
@@ -1,14 +1,22 @@
 const navHeader = document.getElementById("nav-header");
 const heroSectionHeight = document.getElementById("hero").getBoundingClientRect().height;
+const heroSection = document.getElementById("hero");
 
 function stickHeader() {
-	if (navHeader.getBoundingClientRect().top * -1 >= heroSectionHeight) {
-		navHeader.classList.add("fixed-header-js");
-		document.removeEventListener("scroll", stickHeader);
+	if (heroSection.getBoundingClientRect().bottom > 0 && !navHeader.classList.contains("unfix-header-js")) {
+		navHeader.classList.add("unfix-header-js");
+		//250ms is the midpoint of the fade duration 
+		setTimeout(()=>{navHeader.classList.remove("fixed-header-js");}, 250);
+	}
+	else if(heroSection.getBoundingClientRect().bottom <= 0){
+		navHeader.classList.add("fixed-header-js")
+		navHeader.classList.remove("unfix-header-js")
 	}
 }
 
 document.addEventListener("scroll", stickHeader);
+
+
 
 if(window.matchMedia("(max-width: 947px)").matches) {
 	const mobileHamburg = document.getElementById("mobile-hamburg");
